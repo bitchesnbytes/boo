03d29f540f4844ef98d8eed89d5a749698ecd618
kmccormick12 <kendall.mccormick@mail.mcgill.ca>
2015-11-20 22:24:01 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,5 +3,35 @@ import java.io.*;
 
 public class Solution
 {
-  
+
 }
+private static ArrayList<Patient> importPatientInfo(String patientFilePath)
+{
+  ArrayList<Patient> patient = new ArrayList<Patient>();
+  try
+  {
+    BufferedReader reader = new BufferedReader (new FileReader(doctorFilePath));
+    String currentLine = reader.readLine();
+    currentLine = reader.readLine();
+    while(currentLine != null)
+    {
+      String[] parts = new String[4];
+      parts = currentLine.split(",");
+      String firstname = parts[0];
+      String lastname = parts[1];
+      String specialty = parts[2];
+      long id = Long.parseLong(parts[3]);
+      //split each component by commas, and add to a new doctor object 
+      Doctor newdoc = new Doctor(firstname, lastname, specialty, id);
+      doc.add(newdoc);
+      currentLine = reader.readLine();
+    }
+    reader.close();
+    }
+  catch (IOException x)
+  {
+    System.out.println(x);
+  }
+  //returns array list
+  return doc;
+ }
