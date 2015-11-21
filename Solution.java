36c60f273a85305e0715dd9486b2b16bc6f2be23
kaylabranson <kayla.branson@mail.mcgill.ca>
2015-11-21 09:47:10 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -7,9 +7,11 @@ class Patient
   private boolean Remission;
   private double RemDuration;
   private double monthsToLive; 
-  HashMap<String[], Float[]> data = new HashMap<String[], Float[]>();
+  HashMap<String, Float> data = new HashMap<String, Float>();
   
-  public Patient(String patientId, boolean Remission, double RemDuration, double monthsToLive, HashMap data){
+  
+  // Constructor that takes all Patient attributes as input
+  public Patient(String patientId, boolean Remission, double RemDuration, double monthsToLive, HashMap<String, Float> data){
     this.patientId = patientId;
     this.Remission = Remission;
     this.RemDuration = RemDuration;
@@ -56,12 +58,18 @@ public class Solution
   
   public static void main (String[] args)
   {
+    InputStream in;
+    if( args.length > 0 ){
+      in = new FileInputStream( args[0] );}
+    else{
+      in = System.in;}
     ArrayList<Patient> butthole = new ArrayList<Patient>();
-    butthole = importPatientInfo("trainingData.txt");
-    for(int i = 0; i<butthole.size(); i++)
+    patientList = importPatientInfo("trainingData.txt");
+    for(int i = 0; i<patientList.size(); i++)
     {
-      System.out.println(butthole.get(i).getPatientId());
+      System.out.println(patientList.get(i).getPatientId());
     }
+    
   }
   private static ArrayList<Patient> importPatientInfo(String patientFilePath)
   {
@@ -86,47 +94,48 @@ public class Solution
       currentLine = currentLine.toLowerCase();
       parts = currentLine.split("\t");
       
-      
-      
-      int [] nonNumIndex = {1,4,5,6,7,8,9,10,11};
-      
-      for (int m = 1; m < nonNumIndex.length; m++)
-      {
-        int i = nonNumIndex[m];
-        
-        if (parts[i] == "no" || parts[i] == "m" || parts[i] == "neg" || parts[i] == "anthra-hdac")
-        {
-          parts[i] = "0";
-        }
-        
-        if (parts[i] == "yes" || parts[i] == "f" || parts[i] == "pos" || parts[i] == "anthra-plus")
-        {
-          parts[i] = "1";
-        }
-        
-        if (parts[i] == "flu-hdac")
-        {
-          parts[i] = "2";
-        }
-        
-        if (parts[i] == "hdac-plus")
-        {
-          parts[i] = "3";
-        }
-        
-        if (parts[i] == "stdaracplus")
-        {
-          parts[i] = "4";
-        }
-        
-        
-        if (parts[i] == "na" || parts[i] == "notdone")
-        {
-          parts[i] = "44";
-        }
-      }
+
       while(currentLine!= null)
       {
+                currentLine = currentLine.toLowerCase();
+        parts = currentLine.split("\t");
+        int [] nonNumIndex = {1,4,5,6,7,8,9,10,11};
+        
+        for (int m = 0; m < nonNumIndex.length; m++)
+        {
+          
+          int i = nonNumIndex[m];
+          
+          if (parts[i].equals("no") || parts[i].equals("m") || parts[i].equals("neg") || parts[i].equals("anthra-hdac"))
+          {
+            parts[i] = "0";
+          }
+          
+          if (parts[i].equals("yes") || parts[i].equals("f") || parts[i].equals("pos") || parts[i].equals("anthra-plus"))
+          {
+            parts[i] = "1";
+          }
+          
+          if (parts[i].equals("flu-hdac"))
+          {
+            parts[i] = "2";
+          }
+          
+          if (parts[i].equals("hdac-plus"))
+          {
+            parts[i] = "3";
+          }
+          
+          if (parts[i].equals("stdarac-plus"))
+          {
+            parts[i] = "4";
+          }
+          
+          if (parts[i].equals("na") || parts[i].equals("notdone") || parts[i].equals("nd"))
+          {
+            parts[i] = "8008135";
+          }
+        }
         HashMap<String, Float> aData = new HashMap<String, Float>();
         String aID;
         boolean aRemission;
@@ -141,19 +150,34 @@ public class Solution
          {
            aRemission = false;
          }
-         aDuration =Double.parseDouble(parts[267]);
+         if (!(parts[267].equals("na")))
+         {
+           aDuration =Double.parseDouble(parts[267]);
+         }
+         else{ aDuration = 0; }
+         
          aMonths = Double.parseDouble(parts[268]);
          int counter2 = 0;
         for (int i=1; i<266; i++)
         {
+         // System.out.println(parts[i]);
+          if(parts[i].equals("na"))
+          {
+            Float value = Float.parseFloat("8008135");
+          }
+          else
+          {
           Float value = Float.parseFloat(parts[i]);
+          
+//          System.out.println(value);
           aData.put(keys[counter2], value);
+          }
         }
+        
         Patient newPatient = new Patient (aID, aRemission, aDuration, aMonths, aData);
         patient.add(newPatient);
         currentLine = reader.readLine();
-        currentLine = currentLine.toLowerCase();
-        parts = currentLine.split("\t");
+
       }
       reader.close();
     }
@@ -165,3 +189,4 @@ public class Solution
   return patient;
  }
 }
+
