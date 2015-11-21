c8778fb98a4667678da39eb1f91c5fc59d63ef49
kmccormick12 <kendall.mccormick@mail.mcgill.ca>
2015-11-20 22:39:36 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -6,7 +6,6 @@ public class Patient
   private boolean Remission;
   private double RemDuration;
   private double monthsToLive; 
-  HashMap<String[], Float[]> data = new HashMap<String[], Float[]>();
   
   public boolean getRemission(){
     return this.Remission;
@@ -34,6 +33,35 @@ public class Patient
   
 }
 
+private static ArrayList<Patient> importPatientInfo(String patientFilePath)
+{
+  ArrayList<Patient> patient = new ArrayList<Patient>();
+  try
+  {
+    BufferedReader reader = new BufferedReader (new FileReader(patientFilePath));
+    String currentLine = reader.readLine();
+    currentLine = reader.readLine();
+    while(currentLine != null)
+    {
+      String[] parts = new String[4];
+      parts = currentLine.split(",");
+      String firstname = parts[0];
+      String lastname = parts[1];
+      String specialty = parts[2];
+      long id = Long.parseLong(parts[3]);
+      Patient newPatient = new Patient(aRemission, aDuration, aMonths, aCorrelation);
+      doc.add(newdoc);
+      currentLine = reader.readLine();
+    }
+    reader.close();
+    }
+  catch (IOException x)
+  {
+    System.out.println(x);
+  }
+  //returns array list
+  return doc;
+ }
 public class Solution
 {
   
